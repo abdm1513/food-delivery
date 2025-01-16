6c3028ab685e8434bc1090a7ef67d00a220b44e9
abdm1513 <abskm4@gmail.com>
2025-01-15 23:13:48 -0800

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-frontend-vifr.onrender.com'
+export const url = 'https://food-delivery-backend-455u.onrender.com'
