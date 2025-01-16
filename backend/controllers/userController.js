1e0cd2b0e5b1e28428c9ca0c3131ac677d2dadce
abdm1513 <abskm4@gmail.com>
2025-01-15 22:14:44 -0800

    bcrypt to bcryptjs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcryptjs from "bcryptjs"
 import validator from "validator"
 
 const loginUser = async (req,res) => {
@@ -10,7 +10,7 @@ const loginUser = async (req,res) => {
       if (!user) {
           return res.json({success:false,message:"User doesn't exist"})
        }
-          const isMatch = await bcrypt.compare(password,user.password)
+          const isMatch = await bcryptjs.compare(password,user.password)
        
       if (!isMatch) {
             return res.json({success:false,message:"Invalid credentials"})  
@@ -44,8 +44,8 @@ const registerUser = async (req,res) => {
     if(password.length<8) {
         return res.json({success:false,message:"Please enter a strong password"})
     }
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password,salt)
+    const salt = await bcryptjs.genSalt(10)
+    const hashedPassword = await bcryptjs.hash(password,salt)
 
     const newUser = new userModel({
         name:name,
